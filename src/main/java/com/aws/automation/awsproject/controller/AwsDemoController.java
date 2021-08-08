04969ef53bd09c57f65ce1243fbfb9cade06a53f
Chandan Thakur <chandanthakur.ct@gmail.com>
2021-08-08 21:39:57 +0530

    Add files via upload
    1st commit

---
+++ src/main/java/com/aws/automation/awsproject/controller/AwsDemoController.java
@@ -0,0 +1,17 @@
+package com.aws.automation.awsproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class AwsDemoController {
+
+	@GetMapping("/check")
+	public String getHealthCheck() {
+		
+		return "Successfully Run the application...";
+	}
+	
+}
---
+++ src/test/java/com/aws/automation/awsproject/AwsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.automation.awsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
